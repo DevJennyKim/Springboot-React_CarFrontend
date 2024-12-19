ca4f97beb2872a0001e81bd7df433646584bdb38
Haeun[Jenny] Kim <dev.jennyK@gmail.com>
2024-12-18 18:54:39 -0700

    add grid on component

--- src/components/CarList/CarList.tsx
+++ src/components/CarList/CarList.tsx
@@ -1,3 +1,9 @@
+import {
+  DataGrid,
+  gridClasses,
+  GridToolbarContainer,
+  GridToolbarExport,
+} from '@mui/x-data-grid';
 import { useState } from 'react';
 // import Snackbar from '@mui/material/Snackbar';
 interface Car {
@@ -8,12 +14,43 @@ interface Car {
   year: number;
   price: number;
 }
+function CustomToolbar() {
+  return (
+    <GridToolbarContainer className={gridClasses.toolbarContainer}>
+      <GridToolbarExport />
+    </GridToolbarContainer>
+  );
+}
 
 function CarList() {
   const [cars, setCars] = useState<Car[]>([]);
   const [open, setOpen] = useState(false);
 
-  return <div></div>;
+  const columns = [
+    { field: 'brand', headerName: 'Brand', width: 200 },
+    { field: 'model', headerName: 'Model', width: 200 },
+    { field: 'color', headerName: 'Color', width: 200 },
+    { field: 'year', headerName: 'Year', width: 150 },
+    { field: 'price', headerName: 'Price', width: 150 },
+    {
+      field: '_links.car.href',
+      headerName: '',
+      sortable: false,
+      filterable: false,
+    },
+    {
+      field: '_links.self.href',
+      headerName: '',
+      sortable: false,
+      filterable: false,
+    },
+  ];
+
+  return (
+    <>
+      <DataGrid rows={cars} columns={columns}></DataGrid>
+    </>
+  );
 }
 
 export default CarList;
